1e51f0b84ff56097301bb405876c3da947673ed8
Umit KARABACAK <umit.karabacak@hotmail.com.tr>
2019-12-19 17:44:24 +0300

    fatmagül için kommit atıyorum

---
+++ NBUY_MOD/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace NBUY_MOD.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
